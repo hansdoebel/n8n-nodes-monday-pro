dcf0bea37bd6f398189e426e3244378d73b971b4
Hans Doebel <hansdoebel@mailbox.org>
2025-12-05 01:03:58 +0100

    initial monday.com pro node import

---
+++ README.md
@@ -0,0 +1,17 @@
+# n8n-nodes-monday-pro
+
+[n8n](https://n8n.io/) is a [fair-code licensed](https://docs.n8n.io/reference/license/) workflow automation platform.
+
+This is an n8n community node. It lets you use Monday.com in your n8n workflows.
+
+## Resources
+
+- [n8n Website](https://n8n.io/)
+- [n8n community nodes documentation](https://docs.n8n.io/integrations/community-nodes/)
+- [Monday.com Website](https://monday.com/)
+- [Monday.com Open API reference](https://developer.monday.com/api-reference/)
+- [GitHub Repository](https://github.com/hansdoebel/n8n-nodes-monday-pro)
+
+## Version history
+
+- `1.0.0` – Initial release
---
+++ credentials/MondayProApi.credentials.ts
@@ -0,0 +1,47 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from "n8n-workflow";
+
+export class MondayProApi implements ICredentialType {
+	name = "mondayProApi";
+
+	displayName = "Monday.com API";
+
+	documentationUrl = "mondaycom";
+
+	properties: INodeProperties[] = [
+		{
+			displayName: "Token V2",
+			name: "apiToken",
+			type: "string",
+			typeOptions: { password: true },
+			default: "",
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: "generic",
+		properties: {
+			headers: {
+				Authorization: "=Bearer {{$credentials.apiToken}}",
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			headers: {
+				"API-Version": "2025-10",
+				"Content-Type": "application/json",
+			},
+			baseURL: "https://api.monday.com/v2",
+			method: "POST",
+			body: JSON.stringify({
+				query: "query { me { name }}",
+			}),
+		},
+	};
+}
